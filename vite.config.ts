98fa2dd4443c5577bf428546fd7d6053f25f145d
Nithin Manoj <b22cs066@iitj.ac.in>
2026-08-16 17:11:57 +0530

    Prepare calculator for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/goldrategoswamyjewellers/gold-loan-calculator/',
+  base: '/gold-loan-calculator/',
   plugins: [react()],
 });
